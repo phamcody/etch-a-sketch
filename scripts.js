a5783c290e8a9c29563a02ea39bf51e831f8018d
cody <phamgcody01@gmail.com>
2023-04-04 13:23:16 -0400

    Add min-width to grid element so no overflow

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const screenContainer = document.querySelector('#screen');
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 16; i++) {
     const divContainer = document.createElement('div');
     divContainer.classList.add('divContainer');
     screenContainer.appendChild(divContainer);
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 16; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
         grid.textContent="block";
