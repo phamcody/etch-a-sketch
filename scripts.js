f9ebe4006c38f72ba7c4dfa89ba9dc5230057149
cody <phamgcody01@gmail.com>
2023-04-04 13:49:38 -0400

    Make drawing function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 const screenContainer = document.querySelector('#screen');
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 2; i++) {
     const divContainer = document.createElement('div');
     divContainer.classList.add('divContainer');
     screenContainer.appendChild(divContainer);
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= 2; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
         divContainer.appendChild(grid);
     }
 }
+
+const grids = document.getElementsByClassName('grid');
+Array.from(grids).forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('draw');
+    })
+})
+
